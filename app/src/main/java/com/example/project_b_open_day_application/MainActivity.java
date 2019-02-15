8e9084f249be44e032665a5037d40c2d059191fd
Wiebesu <caglar_G@live.nl>
2019-02-15 14:55:00 +0100

    1st commit initiate project

---
+++ app/src/main/java/com/example/project_b_open_day_application/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.project_b_open_day_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
