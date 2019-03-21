f55d3b185a8aa93588bea7e19e7bef6429220fbd
mnarijnm <mnarijnm@gmail.com>
2019-03-21 19:09:44 +0100

    Fixes
    Trying to get it work again

--- app/src/main/java/com/example/project_b_open_day_application/MainActivity.java
+++ app/src/main/java/com/example/project_b_open_day_application/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity
             setSupportActionBar(toolbar);
 
 
+
             DrawerLayout drawer = findViewById(R.id.drawer_layout);
             ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                     this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
