cc1104a71d8a93987608f9d94d54352b94e1ffe7
mnarijnm <mnarijnm@gmail.com>
2019-03-23 00:25:47 +0100

    -Adding all XML/Java activitys

--- app/src/main/java/com/example/project_b_open_day_application/CoursesInformationActivity.java
+++ app/src/main/java/com/example/project_b_open_day_application/CoursesInformationActivity.java
@@ -4,17 +4,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class CoursesActivity extends Fragment {
+public class CoursesInformationActivity extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_courses, container, false);
+        return inflater.inflate(R.layout.activity_courses_information, container, false);
 
     }
 }
