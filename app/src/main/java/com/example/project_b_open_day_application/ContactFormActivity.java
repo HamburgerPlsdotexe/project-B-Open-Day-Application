0abb172e80784b6d9f35466a80ddc98f726ca133
hieu401 <hieu1@hotmail.com>
2019-04-03 10:00:23 +0200

    werkend email functie met prefilled emailadress.

--- app/src/main/java/com/example/project_b_open_day_application/ContactFormActivity.java
+++ app/src/main/java/com/example/project_b_open_day_application/ContactFormActivity.java
@@ -1,5 +1,7 @@
 package com.example.project_b_open_day_application;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,15 +10,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class ContactFormActivity extends Fragment {
+public class ContactFormActivity extends Fragment implements View.OnClickListener {
+
+    private Button sendBtn;
+    private String subjectString;
+    private String nameString;
+    private String emailString;
+    private String messageString;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.fragment_contact_form, container, false);
+        View v = inflater.inflate(R.layout.fragment_contact_form, container, false);
+        EditText subject = v.findViewById(R.id.Inhoud_onderwerpveld);
+        EditText name = v.findViewById(R.id.Inhoud_naamveld);
+        EditText email = v.findViewById(R.id.Inhoud_emailveld);
+        EditText message = v.findViewById(R.id.Inhoud_berichtveld);
+        sendBtn = v.findViewById(R.id.sendmessage);
+
+        this.subjectString = subject.getText().toString();
+        this.emailString = email.getText().toString();
+        this.messageString = message.getText().toString();
+        this.nameString = name.getText().toString();
+
+        sendBtn.setOnClickListener(this);
+        return v;
     }
 
     @Override
@@ -25,4 +49,20 @@ public class ContactFormActivity extends Fragment {
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle(R.string.nav_ContactForm);
     }
+
+    private void sendMail(String subject, String message, String email) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto: service@hr.nl"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        intent.putExtra(Intent.EXTRA_EMAIL, email);
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void onClick(View view){
+        this.sendMail(this.subjectString, this.messageString, "service@hr.nl ");
+    }
+
 }
