b46a9df49208ea441ae7c226b1690848e54efe4d
marijn <mnarijnm@gmail.com>
2019-05-15 13:05:48 +0200

    -Clicking on phone in contact is working

--- app/src/main/java/com/example/project_b_open_day_application/ContactsActivity.java
+++ app/src/main/java/com/example/project_b_open_day_application/ContactsActivity.java
@@ -1,16 +1,22 @@
 package com.example.project_b_open_day_application;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class ContactsActivity extends Fragment {
@@ -37,5 +43,27 @@ public class ContactsActivity extends Fragment {
                 startActivity(intent);
             }
         });
+
+        final TextView PhoneNumber = getView().findViewById(R.id.phoneNumber);
+        PhoneNumber.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                String phone_no= PhoneNumber.getText().toString().replaceAll("-", "");
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:" + phone_no));
+                callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    //request permission from user if the app hasn't got the required permission
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.CALL_PHONE},   //request specific permission from user
+                            10);
+                    return;
+                }
+                else{
+                    startActivity(callIntent);
+                }
+            }
+        });
+
+
     }
 }
