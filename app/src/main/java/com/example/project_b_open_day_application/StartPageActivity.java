4ceba917248d2f81a91886b870dbed98712f6180
Dino Cosic <dino.cosic1999@gmail.com>
2019-06-02 16:02:18 +0200

    Onclickable text is in, just crashing on click. This needs a fix

--- app/src/main/java/com/example/project_b_open_day_application/StartPageActivity.java
+++ app/src/main/java/com/example/project_b_open_day_application/StartPageActivity.java
@@ -9,20 +9,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class StartPageActivity extends Fragment {
 
-    LinearLayout layout1;
-    LinearLayout layout2;
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //returning our layout file
-        //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.fragment_start_page, container, false);
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_start_page, container, false);
 
-    }
+            final TextView textview = view.findViewById(R.id.date_apr);
+            textview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), ItemDetailActivity.class);
+                    startActivity(intent);
+                }
+            });
+            return view;
+        }
+
+
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -31,19 +39,28 @@ public class StartPageActivity extends Fragment {
         getActivity().setTitle(R.string.nav_StartScreen);
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_start_page, container, false);
+    public void onClick(View v){
+        Intent intent; /*
+        switch(v.getId()){
+            case R.id.nav_Courses:
+                fragment = new CoursesActivity();
+                break;
 
-        final Textview textview = view.findViewById(R.id.date_apr);
-        textview.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), ItemDetailActivity.class);
-                startActivity(intent);
-            }
-        });
-        return view;
+            case R.id.date_mei:
+                Context context = View.getContext();
+                Intent intent = new Intent(context, com.example.project_b_open_day_application.ItemDetailActivity.class);
+                intent.putExtra("DesiredFragment", "Mei");
+                context.startActivity(intent);
+                break;
+
+            case R.id.date_apr:
+                context = View.getContext();
+                intent = new Intent(context, com.example.project_b_open_day_application.ItemDetailActivity.class);
+                intent.putExtra("DesiredFragment", "April");
+                context.startActivity(intent);
+                break;
+
+        }
+        startActivity(intent); */
     }
 }
