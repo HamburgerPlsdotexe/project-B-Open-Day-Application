36550b6574ff1d60b7c806a1ce7c90cd4d976304
Dino Cosic <dino.cosic1999@gmail.com>
2019-06-02 18:48:27 +0200

    1st on click on the startscreen is working. Now working on the dates.

--- app/src/main/java/com/example/project_b_open_day_application/StartPageActivity.java
+++ app/src/main/java/com/example/project_b_open_day_application/StartPageActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,18 +15,20 @@ import android.widget.TextView;
 
 public class StartPageActivity extends Fragment {
 
-
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, @Nullable Bundle savedInstanceState) {
             View view = inflater.inflate(R.layout.fragment_start_page, container, false);
 
-            final TextView textview = view.findViewById(R.id.date_apr);
+            final TextView textview = view.findViewById(R.id.nav_Courses);
             textview.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent intent = new Intent(getActivity(), ItemDetailActivity.class);
-                    startActivity(intent);
+                   Fragment courses = new CoursesActivity();
+                    FragmentManager fm = getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fm.beginTransaction();
+                    fragmentTransaction.replace(R.id.content_frame, courses);
+                    fragmentTransaction.commit();
                 }
             });
             return view;
@@ -37,30 +41,5 @@ public class StartPageActivity extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle(R.string.nav_StartScreen);
-    }
-
-    public void onClick(View v){
-        Intent intent; /*
-        switch(v.getId()){
-            case R.id.nav_Courses:
-                fragment = new CoursesActivity();
-                break;
-
-            case R.id.date_mei:
-                Context context = View.getContext();
-                Intent intent = new Intent(context, com.example.project_b_open_day_application.ItemDetailActivity.class);
-                intent.putExtra("DesiredFragment", "Mei");
-                context.startActivity(intent);
-                break;
-
-            case R.id.date_apr:
-                context = View.getContext();
-                intent = new Intent(context, com.example.project_b_open_day_application.ItemDetailActivity.class);
-                intent.putExtra("DesiredFragment", "April");
-                context.startActivity(intent);
-                break;
-
-        }
-        startActivity(intent); */
     }
 }
