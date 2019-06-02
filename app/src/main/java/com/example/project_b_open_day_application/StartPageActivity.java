8159bc5bba4967eb09f92e3a44f6f42f3016ffe8
Dino Cosic <dino.cosic1999@gmail.com>
2019-06-02 15:02:44 +0200

    Have to fix startpage

--- app/src/main/java/com/example/project_b_open_day_application/StartPageActivity.java
+++ app/src/main/java/com/example/project_b_open_day_application/StartPageActivity.java
@@ -1,5 +1,7 @@
 package com.example.project_b_open_day_application;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -21,5 +23,30 @@ public class StartPageActivity extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle(R.string.nav_StartScreen);
+    }
+
+    public void click(View v){
+        Intent intent;
+        switch(v.getId()){
+            case R.id.nav_Courses:
+                fragment = new CoursesActivity();
+                break;
+
+            case R.id.date_mei:
+                Context context = view.getContext();
+                Intent intent = new Intent(context, com.example.project_b_open_day_application.ItemDetailActivity.class);
+                intent.putExtra("DesiredFragment", "Mei");
+                context.startActivity(intent);
+                break;
+
+            case R.id.date_apr:
+                context = view.getContext();
+                intent = new Intent(context, com.example.project_b_open_day_application.ItemDetailActivity.class);
+                intent.putExtra("DesiredFragment", "April");
+                context.startActivity(intent);
+                break;
+
+        }
+        startActivity(intent);
     }
 }
