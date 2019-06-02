8607fc574e8f73bea204b61905703e8a6ccb78d1
Dino Cosic <dino.cosic1999@gmail.com>
2019-06-02 15:45:20 +0200

    Have to fix startpage

--- app/src/main/java/com/example/project_b_open_day_application/StartPageActivity.java
+++ app/src/main/java/com/example/project_b_open_day_application/StartPageActivity.java
@@ -8,14 +8,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class StartPageActivity extends Fragment {
+
+    LinearLayout layout1;
+    LinearLayout layout2;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
         return inflater.inflate(R.layout.fragment_start_page, container, false);
+
     }
 
     @Override
@@ -25,28 +31,19 @@ public class StartPageActivity extends Fragment {
         getActivity().setTitle(R.string.nav_StartScreen);
     }
 
-    public void click(View v){
-        Intent intent;
-        switch(v.getId()){
-            case R.id.nav_Courses:
-                fragment = new CoursesActivity();
-                break;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_start_page, container, false);
 
-            case R.id.date_mei:
-                Context context = view.getContext();
-                Intent intent = new Intent(context, com.example.project_b_open_day_application.ItemDetailActivity.class);
-                intent.putExtra("DesiredFragment", "Mei");
-                context.startActivity(intent);
-                break;
-
-            case R.id.date_apr:
-                context = view.getContext();
-                intent = new Intent(context, com.example.project_b_open_day_application.ItemDetailActivity.class);
-                intent.putExtra("DesiredFragment", "April");
-                context.startActivity(intent);
-                break;
-
-        }
-        startActivity(intent);
+        final Textview textview = view.findViewById(R.id.date_apr);
+        textview.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ItemDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
